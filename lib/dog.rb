ca8315ca9813caaba84adbe42b894131fe7cb519
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-17 20:14:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Add your code here
+require 'pry'
+
+class Dog
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each{|name| puts "name"}
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.clear_all
+    @@all.delete
+  
+end
